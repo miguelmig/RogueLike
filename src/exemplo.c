f0a1fc594654678bbcb910fef67f8ec60477e740
Miguel Oliveira <miguelmigpt@gmail.com>
2017-05-21 14:49:39 +0100

    O dano dos inimigos escala com o nível atual.

--- src/exemplo.c
+++ src/exemplo.c
@@ -322,8 +322,8 @@ void atacar_jogador(INIMIGO* enemy, ESTADO* e, int* damage_done)
 	}
 
 	//int jogador_health = e->jog.current_health;
-	*damage_done = 10;
-	e->jog.current_health -= 10;
+	*damage_done = min(PLAYER_MAX_HEALTH, e->level * 10);
+	e->jog.current_health -= *damage_done;
 }
 
 void mover_inimigos(ESTADO* e, int* damage_done)
