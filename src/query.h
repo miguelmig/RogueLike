d35f505e0f7bcec6ef68320e4b9d4f3805e054ce
Miguel Oliveira <miguelmigpt@gmail.com>
2017-06-11 00:43:36 +0100

    Documentação parte 3

--- src/query.h
+++ src/query.h
@@ -10,7 +10,33 @@
 */
 int parse_query(const char* query_string, ESTADO* e, int* change_turn);
 
+
+/**
+\brief Cria uma hiperligação para uma ação de movimento
+@param dx Alteração da posição do jogador na coordenada x
+@param dy Alteração da posição do jogador na coordenada y
+@param destination Variável que vai guardar a string da hiperligação
+*/
 void create_move_query(int dx, int dy, char* destination);
+
+/**
+\brief Cria uma hiperligação para uma ação de saída de nível
+@param destination Variável que vai guardar a string da hiperligação
+*/
 void create_exit_query(char* destination);
+
+/**
+\brief Cria uma hiperligação para uma ação de ataque
+@param dx Diferença em x do inimigo que o jogador vai atacar
+@param dy Diferença em y do inimigo que o jogador vai atacar
+@param destination Variável que vai guardar a string da hiperligação
+*/
 void create_attack_query(int dx, int dy, char* destination);
+
+/**
+\brief Cria uma hiperligação para uma ação de usar uma poção
+@param dx Diferença em x da poção que o jogador vai usar
+@param dy Diferença em y da poção que o jogador vai usar
+@param destination Variável que vai guardar a string da hiperligação
+*/
 void create_potion_query(int dx, int dy, char* destination);
